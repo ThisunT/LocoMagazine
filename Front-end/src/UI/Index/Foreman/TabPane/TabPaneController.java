77b3fe5965a33fe038fff2a34b223012ee122bf0
Piumi Lokuge <piumi1993indeewari@gmail.com>
2017-09-22 00:08:24 +0530

    Foreman-Main Tab Pane

---
+++ Front-end/src/UI/Index/Foreman/TabPane/TabPaneController.java
@@ -0,0 +1,7 @@
+package UI.Index.Foreman.TabPane;
+
+/**
+ * Created by piumiindeevari on 9/21/2017.
+ */
+public class TabPaneController {
+}
---
+++ Front-end/src/UI/Index/Foreman/TabPane/TabPaneMain.java
@@ -0,0 +1,25 @@
+package UI.Index.Foreman.TabPane;/**
+ * Created by piumiindeevari on 9/21/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TabPaneMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("tabPane.fxml"));
+        primaryStage.setScene(new Scene(root, 1280, 800));
+        primaryStage.show();
+    }
+}
